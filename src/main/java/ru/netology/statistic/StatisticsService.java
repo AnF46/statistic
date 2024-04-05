2642b63cdbd1852b7bc81452c9324715b40437a1
AnF <anfwork007@gmail.com>
2024-04-05 19:16:58 +0300

    added new test

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -2,10 +2,10 @@ package ru.netology.statistic;
 
 public class StatisticsService {
     public long findMax(long[] incomes) {
-        long currentMax = incomes[0];
-        for (long income : incomes) {
-            if (currentMax < income) {
-                currentMax = income;
+        long currentMax = incomes[0]; //текущий максимум = значению 1ой ячейки массива
+        for (long income : incomes) {   // переменная ПОСТУПЛЕНИЕ принимает новое значение из массива
+            if (currentMax < income) {  // если текущий максим. меньше поступления,
+                currentMax = income;    // текущий макс. равен поступлению
             }
         }
         return currentMax;
